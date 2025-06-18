3f5313d798bec380e18e3f249518de6bb7471c01
desp0o <tornike.despotashvili@gmail.com>
2025-06-18 23:55:55 +0400

    add loading state

--- crypto-glass/Screens/MainScreen/MainScreen.swift
+++ crypto-glass/Screens/MainScreen/MainScreen.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MainScreen: View {
   @State private var vm = MainScreenViewModel()
+  @State private var isVisible = false
   var body: some View {
     GlassEffectContainer {
       ZStack {
@@ -16,16 +17,33 @@ struct MainScreen: View {
         
         ScrollView {
           VStack(spacing: 30) {
-            
             NFTView(vm: vm)
             LastListedCrypto(vm: vm)
             
+            Button("click") {
+              isVisible.toggle()
+            }
           }
         }
       }
       .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
       .padding(.top, 20)
       .background(.bgCol)
+      .sheet(isPresented: $isVisible) {
+        ZStack{
+          BlurEllipses()
+          Text("test")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea()
+        .presentationDetents([.medium])
+      }
+      .onAppear(perform: {
+        print(vm.isLoading)
+      })
+      .onChange(of: vm.isLoading) {
+        print(vm.isLoading)
+      }
     }
   }
 }
--- crypto-glass/Screens/MainScreen/Views/LastListedCrypto.swift
+++ crypto-glass/Screens/MainScreen/Views/LastListedCrypto.swift
@@ -17,38 +17,55 @@ struct LastListedCrypto: View {
         .customStyle(size: 22, weight: .bold)
         .padding(.leading, 20)
       
-      ScrollView(.horizontal) {
-        LazyHStack(spacing: 20) {
-          ForEach(vm.latestListedCrypto, id: \.id) { crypto in
-            VStack {
-              KFImage(URL(string: "https://s2.coinmarketcap.com/static/img/coins/32x32/\(crypto.id).png"))
-              
-              HStack {
-                Text(crypto.name)
-                  .customStyle(weight: .bold)
+      if !vm.isLoading {
+        ScrollView(.horizontal) {
+          LazyHStack(spacing: 20) {
+            ForEach(vm.latestListedCrypto, id: \.id) { crypto in
+              VStack {
+                KFImage(URL(string: "https://s2.coinmarketcap.com/static/img/coins/32x32/\(crypto.id).png"))
                 
-                Spacer()
-              }
-              
-              HStack {
-                Text("Price")
-                  .customStyle(size: 14, weight: .bold)
+                HStack {
+                  Text(crypto.name)
+                    .customStyle(weight: .bold)
+                  
+                  Spacer()
+                }
                 
-                Text(String(format: "%.2f", crypto.quote.USD.price))
-                  .customStyle(size: 14, weight: .bold)
-                Spacer()
+                HStack {
+                  Text("Price")
+                    .customStyle(size: 14, weight: .bold)
+                  
+                  Text("\(String(format: "%.2f", crypto.quote.USD.price)) USD")
+                    .customStyle(size: 14, weight: .bold)
+                  Spacer()
+                }
+                
               }
-              
+              .padding(10)
+              .glassEffect(in: RoundedRectangle(cornerRadius: 22))
             }
-            .padding(10)
-            .glassEffect(in: RoundedRectangle(cornerRadius: 22))
           }
+          .padding(.horizontal, 20)
+          .scrollTargetLayout()
         }
-        .padding(.horizontal, 20)
-        .scrollTargetLayout()
+        .scrollIndicators(.hidden)
+        .scrollTargetBehavior(.viewAligned)
+      } else {
+        ScrollView(.horizontal) {
+          HStack(spacing: 20) {
+            ForEach(0...3, id: \.self) { _ in
+              VStack {
+                ProgressView()
+                  .scaleEffect(1.5)
+                  .tint(.elipse2Col)
+              }
+              .frame(width: 180, height: 100)
+              .glassEffect(in: RoundedRectangle(cornerRadius: 22))
+            }
+          }
+          .padding(.horizontal, 20)
+        }
       }
-      .scrollIndicators(.hidden)
-      .scrollTargetBehavior(.viewAligned)
     }
   }
 }
